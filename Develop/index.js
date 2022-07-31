7301cf6ec29a6f62c23cfe82ce6da1d84d7853ef
Liliana Perez <103470899+o7lili@users.noreply.github.com>
2022-07-31 10:24:47 -0400

    capture input with command-line arguments

--- Develop/index.js
+++ Develop/index.js
@@ -12,5 +12,19 @@
 // Function call to initialize app
 // init();
 
-var readmeDataArgs = process.argv.slice(2, process.argv.length);
+const readmeDataArgs = process.argv.slice(2, process.argv.length);
 console.log(readmeDataArgs);
+
+const printReadMeData = readmeDataArr => {
+    for (let i = 0; i < readmeDataArr.length; i += 1) {
+        console.log(readmeDataArr[i]);
+    }
+
+    console.log('================');
+
+    readmeDataArr.forEach((readmeItem) => {
+        console.log(readmeItem)
+    });
+};
+
+printReadMeData(readmeDataArgs);
