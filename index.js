7748d85197a343403c5951ae5d29ac89923d0227
Liliana Perez <103470899+o7lili@users.noreply.github.com>
2022-08-01 10:15:15 -0400

    instal and test inquirer

--- index.js
+++ index.js
@@ -1,25 +1,23 @@
+const inquirer = require('inquirer');
+// const fs = require('fs');
+// const generateReadMe = require('./src/README-template');
 
-const fs = require('fs');
-const generateReadMe = require('./src/README-template');
+// // TODO: Create an array of questions for user input
+// const pageREADME = generateReadMe(projectTitle, projectDesc);
 
-// TODO: Create an array of questions for user input
-const readmeDataArgs = process.argv.slice(2);
 
-const [projectTitle, projectDesc] = readmeDataArgs;
+// fs.writeFile('./README.md', pageREADME, err => {
+//     if (err) throw err;
 
-// const questions = [];
+//     console.log('README complete, check out README.md to see the output.');
+// });
 
-// TODO: Create a function to write README file
-// function writeToFile(fileName, data) {}
-
-// TODO: Create a function to initialize app
-// function init() {}
-
-// Function call to initialize app
-// init();
-
-fs.writeFile('./README.md', generateReadMe(projectTitle, projectDesc), err => {
-    if (err) throw err;
-
-    console.log('README complete, check out README.md to see the output.');
-});
+inquirer
+  .prompt([
+    {
+        type: 'input',
+        name: 'name',
+        message: 'What is your name?'
+    }
+  ])
+  .then(answers => console.log(answers));
