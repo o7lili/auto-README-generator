3bc35a90bd95731dfd01466d0d76eab62dc2585c
Liliana Perez <103470899+o7lili@users.noreply.github.com>
2022-08-01 22:02:37 -0400

    add validation to prompts

--- index.js
+++ index.js
@@ -17,12 +17,28 @@ const promptUser = () => {
         {
             type: 'input',
             name: 'username',
-            message: 'What is your GitHub username?'
+            message: 'What is your GitHub username?',
+            validate: nameInput => {
+                if (nameInput) {
+                    return true;
+                } else {
+                    console.log('Please enter a username');
+                    return false;
+                }
+            }
         },
         {
             type: 'input',
             name: 'email',
-            message: 'What is your email address?'
+            message: 'What is your email address?',
+            validate: nameInput => {
+                if (nameInput) {
+                    return true;
+                } else {
+                    console.log('Please enter an email address');
+                    return false;
+                }
+            }
         }
     ]);
 }
@@ -42,22 +58,54 @@ const promptProject = projectData => {
             {
                 type: 'input',
                 name: 'Project Title',
-                message: 'What is the title of your project?'
+                message: 'What is the title of your project?',
+                validate: nameInput => {
+                    if (nameInput) {
+                        return true;
+                    } else {
+                        console.log('Please enter the title of your project');
+                        return false;
+                    }
+                }
             },
             {
                 type: 'input',
                 name: 'Project Description',
-                message: 'Provide a description of your project:'
+                message: 'Provide a description of your project:',
+                validate: nameInput => {
+                    if (nameInput) {
+                        return true;
+                    } else {
+                        console.log('Please enter a description of your project');
+                        return false;
+                    }
+                }    
             },
             {
                 type: 'input',
                 name: 'Installation',
-                message: 'Steps to install the project:'
+                message: 'Steps to install the project:',
+                validate: nameInput => {
+                    if (nameInput) {
+                        return true;
+                    } else {
+                        console.log('Please list steps to install your project');
+                        return false;
+                    }
+                }   
             },
             {
                 type: 'input',
                 name: 'Usage',
-                message: 'Instructions and examples for use of your project:'
+                message: 'Instructions and examples for use of your project:',
+                validate: nameInput => {
+                    if (nameInput) {
+                        return true;
+                    } else {
+                        console.log('Please provide instructions for use of your project');
+                        return false;
+                    }
+                }    
             },
             {
                 type: 'list',
@@ -68,12 +116,28 @@ const promptProject = projectData => {
             {
                 type: 'input',
                 name: 'Contributing',
-                message: 'Provide guidelines for how other developers can contribute to your project:'
+                message: 'Provide guidelines for how other developers can contribute to your project:',
+                validate: nameInput => {
+                    if (nameInput) {
+                        return true;
+                    } else {
+                        console.log('Please provide instructions on how to contribute to your project');
+                        return false;
+                    }
+                }    
             },
             {
                 type: 'input',
                 name: 'Tests',
-                message: 'Provide instructions and examples on how to run tests on your project:'
+                message: 'Provide instructions and examples on how to run tests on your project:',
+                validate: nameInput => {
+                    if (nameInput) {
+                        return true;
+                    } else {
+                        console.log('Please provide instructions on how to run tests on your project');
+                        return false;
+                    }
+                }    
             }
         ]);
 };
